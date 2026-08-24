acf643b41f3e7300e51187bd3978ad4dd929049f
yinhui <yinhui.ame@bytedance.com>
2026-08-24 14:07:21 +0800

    chore: release v1.0.7

--- client/option/name_option.go
+++ client/option/name_option.go
@@ -4,7 +4,7 @@ import "github.com/douyinpay/douyinpay-go/client"
 
 const (
 	// 每次sdk升级，需要更新版本号
-	ClientVersion = "GO-v1.0.6"
+	ClientVersion = "GO-v1.0.7"
 )
 
 func WithClientAgentName(agentName string, mchID string) client.ClientOption {
